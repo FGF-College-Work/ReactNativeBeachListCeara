b8fe4a15030b8c1d4dedcf3ee405180470d99abf
marcialwushu <marcialwushu@gmail.com>
2018-08-24 11:43:03 -0300

    itens with image links in const list added

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -16,49 +16,49 @@ const list = [
       subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia de Ponta Grossa, Icapuí',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Praia-Majorlandia-Canoa-Quebrada-1.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia de Jericoacoara, Jijoca de Jericoacoara',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Praia-Jericoacoara-Ceara-1.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia da Lagoa do Paraíso, Jericoacoara',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Lagoa-Paraiso-Jericoacoara-1.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia de Cumbuco, Caucaia',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Praia-Ceara-Cumbuco-1.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia da Lagoinha, Paraipaba',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Praia-Lagoinha-Ceara-1.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia da Lagoa Azul, Jijoca de Jericoacoara ',
+      avatar_url: 'https://media-cdn.tripadvisor.com/media/photo-s/0d/8a/61/9e/redinhas.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia do Futuro, Fortaleza',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Praia-do-Futuro-Ceara.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia do Meireles ou Praia de Iracema, Fortaleza',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Fortaleza-Praia-Meireles.jpg',
+      subtitle: 'Ceara'
     },
     {
-      name: '',
-      avatar_url: '',
-      subtitle: ''
+      name: 'Praia de Bitupitá, Barroquinha',
+      avatar_url: 'https://viagenscinematograficas.com.br/wp-content/uploads/2015/01/Bitupita-Ceara-1.jpg',
+      subtitle: 'Ceara'
     },
     
   ]
